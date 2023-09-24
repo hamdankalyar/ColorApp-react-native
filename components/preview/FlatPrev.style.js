3ae2304c4d248e86d0d3ac81e3b63a73f39fb688
Hamdan kalyar <hamdan.kalyar@gmail.com>
2023-09-24 15:36:13 +0500

    partial functionality of adding palette

--- components/preview/FlatPrev.style.js
+++ components/preview/FlatPrev.style.js
@@ -3,9 +3,13 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   dark: {
     color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   white: {
     color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 export default styles;
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,21 @@
-import { View, Text, TouchableOpacity, FlatList,RefreshControl } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  RefreshControl,
+} from 'react-native';
 import React, { useCallback, useEffect, useState, useContext } from 'react';
 import ColorBox from '../components/box/colorbox/ColorBox';
 import FlatPrev from '../components/preview/FlatPrev';
 import ThemeContext from '../Hook/ThemeContext';
 
-export default function Home({ navigation }) {
+export default function Home({ navigation, route }) {
+  const newColorPalette = route.params
+    ? route.params.newColorPalette
+    : undefined;
   const [color, setColor] = useState([]);
-  const [isRefreshing,setIsRefreshing] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
   const handleFetchPalettes = useCallback(async () => {
     const response = await fetch(
       'https://color-palette-api.kadikraman.vercel.app/palettes',
@@ -19,34 +28,50 @@ export default function Home({ navigation }) {
   useEffect(() => {
     handleFetchPalettes();
   }, []);
-const handleRefresh = useCallback(async () => {
-  setIsRefreshing(true)
- 
-  await handleFetchPalettes()
-  setTimeout(() => {
-    setIsRefreshing(false)
-  }, 1000);
-},[])
+  useEffect(() => {
+    if (newColorPalette) {
+      setColor((color) => [newColorPalette, ...color]);
+    }
+  }, [newColorPalette]);
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+
+    await handleFetchPalettes();
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 1000);
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-       
         data={color}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <FlatPrev colorArr={item} navigation={navigation} />
         )}
-        refreshing={isRefreshing} 
+        refreshing={isRefreshing}
         onRefresh={handleRefresh}
         ListHeaderComponent={
-          <TouchableOpacity onPress={()=>{navigation.navigate('ColorPaletteModal')}}>
-            <Text>Launch the modal </Text>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('ColorPaletteModal');
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                color: 'teal',
+                alignSelf: 'center',
+                marginVertical: 10,
+              }}
+            >
+              Add Color Palette
+            </Text>
           </TouchableOpacity>
         }
-        
       />
-
     </View>
   );
 }
